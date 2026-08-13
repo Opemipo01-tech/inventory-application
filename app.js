b86d69de7943da7ee9f5cb8cee4302b224c8db56
Opemipo01-tech <ahmad001mahmud@gmail.com>
2026-08-13 18:29:50 +0100

    fix internal server error bug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use("/",indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
     console.log("Running");
